84b5139c258a8bfd0fe19b8b663381b41072e564
Pilar <p.garciacampo@gmail.com>
2020-08-24 18:34:04 +0200

    Finish 20 add auth app

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,4 +1,5 @@
 export { default as UnAuthPage } from "./UnAuthPage";
+export { default as AuthPage } from "./AuthPage";
 export { default as Button } from "./Button";
 export { default as Label } from "./Label";
 export { default as Input } from "./Input";
